49ffd083761aa34e8058ce3bac6cf27466d398a4
Seth Starr <starrseth@hotmail.com>
2020-06-16 16:00:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 end
 
 def list_dwarves(array)
-  array(Gimli Thorin Balin Gloin).each_with_index | 1. , 
+  array(Gimli Thorin Balin Gloin).each_with_index | Item , Index |
+  
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
